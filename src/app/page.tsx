c243b9cd0f7a25a4439c71b761059f2472c2ec9d
hossein naseri <hossein.naseri>
2024-12-30 11:21:19 +0330

    fix autplay issue and responsivness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import React from 'react';
 export default function Home() {
 
   return (
-    <div className='min-h-screen px-5'>
-      <Typography className="p-3" variant='heading'>Hossein Naseri&apos;s Interview Test Result </Typography>
+    <div className='min-h-screen px-5 pt-2 pb-10'>
+      <Typography className="p-3 pb-4" variant='heading'>Hossein Naseri&apos;s Interview Test Result </Typography>
       <ProductsModule />
     </div>
   );
--- src/modules/ProductsModule/ProductsModule.tsx
+++ src/modules/ProductsModule/ProductsModule.tsx
@@ -9,19 +9,42 @@ import { CardContents } from './ProductsModule.types';
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/thumbs";
+import { Autoplay } from 'swiper/modules';
 
 export default function ProductsModule() {
 
   return (
     <div>
       <Swiper
-        slidesPerView={2}
         spaceBetween={50}
         loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        speed={5000}
+        modules={[Autoplay]}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            loop: true,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+            loop: true,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+            loop: true,
+          },
+        }}
       >
 
         {
-          (data as CardContents)?.map((card, index) => (<SwiperSlide key={index}>
+          ([...data, ...data] as CardContents)?.map((card, index) => (<SwiperSlide key={index}>
             <Card
               key={index}
               img={card.backgroundMedia?.src}
